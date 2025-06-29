27cd9580d2cbda8fc94f561b1e86411e48f32012
jayden-creat3 <jaydenkamau508@gmail.com>
2025-06-29 17:59:16 +0300

    Add initial implementation of cat array functions

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+// Initial array of cats
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive methods
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats; // Return the modified array
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats; // Return the modified array
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats; // Return the modified array
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats; // Return the modified array
+}
+
+// Nondestructive methods
+function appendCat(name) {
+  return [...cats, name]; // Return a new array with the cat added
+}
+
+function prependCat(name) {
+  return [name, ...cats]; // Return a new array with the cat added
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); // Return a new array without the last cat
+}
+
+function removeFirstCat() {
+  return cats.slice(1); // Return a new array without the first cat
+}
+
